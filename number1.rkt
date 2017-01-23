63082913036c73d87d7e3669ef637f8548a6773b
Subhay Manandhar <subhay@Subhays-MacBook-Pro.local>
2017-01-23 00:05:16 -0500

    functions test

--- number1.rkt
+++ number1.rkt
@@ -11,3 +11,21 @@
 
 
 
+(define (factorial x)
+  (IF (= 0 x)
+      1
+      (* x (factorial (- x 1)))
+      )
+  )
+
+(define (fib x)
+  (COND
+   ((= x 0) 0)
+   ((= x 1) 1)
+   ((> x 1) (+ (fib(- x 1)) (fib(- x 2))))
+   (ELSE (DISPLAY "NOT VALID"))
+   )
+  )
+
+
+   
