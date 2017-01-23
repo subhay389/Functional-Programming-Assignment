6d388115673b9abd2a0fb684485de5c1b735eb44
Subhay Manandhar <subhay@Subhays-MacBook-Pro.local>
2017-01-22 19:39:21 -0500

    testing functions and symbol

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Functional-Programming-Assignment
+
+Functional Programming Language Assignment 1. 
+Dr. Li
+
---
+++ number1.rkt
@@ -0,0 +1,13 @@
+(* 3 3)
+
+(define exponent
+  2.718)
+
+(define (expo)
+  2.718)
+
+(define (add x y)
+  (+ x y))
+
+
+
