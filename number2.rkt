e249c3ec1f09d898637f51f6213b249c7c4c5d0e
Subhay Manandhar <subhay@Subhays-MacBook-Pro.local>
2017-01-25 00:48:47 -0500

    working sup-up-number-simple function

--- number2.rkt
+++ number2.rkt
@@ -1,10 +1,13 @@
+;main function to find the sum 
 (define (sum-up-numbers-simple L)
   (COND
    ((NULL? L) 0)
-   ((+ (car L) (sum-up-numbers-simple (cdr L))))
+   ((NUMBER? (car L)) (+ (car L) (sum-up-numbers-simple (cdr L))))
+   (else (sum-up-numbers-simple (cdr L)))
    )
   )
 
-(display (number? '1))
-(display (number? '()))
+
+;(display (number? '1))
+;(display (number? '()))
          
