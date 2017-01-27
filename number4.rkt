f742d8359363b65acde11d777f693381133f3f40
Subhay Manandhar <subhay@Subhays-MacBook-Pro.local>
2017-01-27 13:12:14 -0500

    handle base case

--- number4.rkt
+++ number4.rkt
@@ -1,8 +1,8 @@
 
-(define (find-min L)
+(DEFINE (find-min L)
   
   (COND
-   ((NULL? L) 999999999)
+   ((NULL? L) 99999999999)
    ;ignoring non integers
    ((NOT (number? (car L))) (find-min (cdr L)))
    ;base case for returning
@@ -14,6 +14,16 @@
     (find-min (CDR L)))
    )
   )
+
+(DEFINE (min-above-min L1 L2)
+        (COND
+         ;if the list1 is empty 
+         ((NULL? L1) #F)
+         ;if the list2 is empty
+         ((NULL? L2) (find-min L1))
+         )
+        )
+
 
 
 
