6f98d737fbf63c5d1f6a1de025ef6e0cd24ccce6
Subhay Manandhar <subhay@Subhays-MacBook-Pro.local>
2017-01-27 03:05:40 -0500

    recursive call to check all number in list

--- number4.rkt
+++ number4.rkt
@@ -11,6 +11,8 @@
    ;ignoring non integers
    ((not (number? (car L))) (find-min (cdr L)))
    ((number? (car L)))
+   ;recursion to compare all elemtns in list 
+   ((< (car L) (find-min (cdr L))) (car L))
 
     )
    (else
